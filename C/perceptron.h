f0a62ac00d3dad5306c48c94b9c4a2b77d196563
DelSquared <37172200+DelSquared@users.noreply.github.com>
2019-05-18 14:43:26 +0200

    Update perceptron.h

--- C/perceptron.h
+++ C/perceptron.h
@@ -2,22 +2,29 @@
 #define d(x) x*(1-x)
 #define E(x,y) (x-y)*(x-y)
 #define dE(x,y) (y-x)
+//macro definitions to clean up the code
 
 double dt = 0.001;
 unsigned int epochs = 10000000;
 unsigned int verbosity = 1000000;
+//hyperparameters, these can be controlled from main.c if header is not precompiled
 
-typedef struct p1 Perceptron;
+//The perceptron will be defined such that it could be used in main.c as a sort of "object"
+
+//-----------------------------------------------------------------------------------------------
+typedef struct p1 Perceptron; 
 
 typedef double (*MethodDouble)(Perceptron* self, double x[2]);
-typedef void (*MethodVoid)(Perceptron* self, double data[4][2],double label[4]);
+typedef void (*MethodVoid)(Perceptron* self, double data[4][2],double label[4]); //Used to hook up functions to the structs
 
 struct p1 {
    double w[2];
    double b;
-   MethodDouble Eval;
-   MethodVoid Train;
+   MethodDouble Eval; //placeholder for the Eval() function
+   MethodVoid Train; //placeholder for the Train() function
 };
+//Lots of circular definitions here unfortunately but it's required since the typedefs depend on eachother
+//-----------------------------------------------------------------------------------------------
 
 double Eval(Perceptron* self, double x[2]){ 
     return 1.0/(1.0+exp(-(x[0]*(self->w[0])+x[1]*(self->w[1])+(self->b))));
@@ -44,6 +51,8 @@ void Train (Perceptron* self, double data[4][2],double label[4]){
     }
   }
 }
+//Method definitions
+//-----------------------------------------------------------------------------------------------
 
 void Init(Perceptron* p){
    p->w[0]=(double)(rand()%1000)/1000;
@@ -53,3 +62,5 @@ void Init(Perceptron* p){
    p->Train = Train;
    printf("Perceptron initialised\n");
 }
+//Init function to hook methods up to the struct as well as initialise its values
+//-----------------------------------------------------------------------------------------------
