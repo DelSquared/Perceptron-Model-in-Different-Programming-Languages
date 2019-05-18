5e8c179de3c6d67b586b4cdfd4c972e4e7282676
DelSquared <37172200+DelSquared@users.noreply.github.com>
2019-05-18 14:47:56 +0200

    Update main.c

--- C/main.c
+++ C/main.c
@@ -1,33 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "perceptron.h"
-#define SKIP_LINE printf("\n") //this was defined because I couldn't be bothered to keep typing it out over and over just to make the output "pretty"
+#define SKIP_LINE printf("\n") 
+//this macro was defined because I couldn't be bothered to keep
+//...typing it out over and over just to make the output "pretty"
 
 int main(void) {
 
   double x[4][2]={{0,0},{0,1},{1,0},{1,1}};
-  double y[4]={0,0,0,1};
-
+  double y[4]={0,0,0,1}; //AND gate
+  
   unsigned short j;
+  //loop variable for printing purposes because C does not let you define it in the loop
 
   Perceptron p;
-  Init(&p);
+  Init(&p); //defining and initialising the perceptron
+  //---------------------------------------
   
   printf("\nPre-Training\n");
   for(j=0;j<4;j++){
     printf("%f\n",p.Eval(&p, x[j]));
   }
+  SKIP_LINE; //printing pre-training values
+  //---------------------------------------
+  
+  p.Train(&p,x,y); //training
   SKIP_LINE;
+  //---------------------------------------
 
-  p.Train(&p,x,y);
-
-  SKIP_LINE;
-
-  printf("\nPre-Training\n");
+  printf("\nPost-Training\n");
   for(j=0;j<4;j++){
     printf("%f\n",p.Eval(&p, x[j]));
   }
-  SKIP_LINE;
+  SKIP_LINE; //printing post-training values
+  //---------------------------------------
   
 
   return 0;
